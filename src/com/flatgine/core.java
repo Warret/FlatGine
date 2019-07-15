0f2fba28b09a73ae5b163cad7a24d6b4ca9c2dec
unknown <shihalev0187@mail.ru>
2019-07-15 17:10:36 +0400

    add func hasCollisionComplex

--- src/com/flatgine/core.java
+++ src/com/flatgine/core.java
@@ -11,12 +11,16 @@ public class core {
 
         else{return false;}
     }
-    public static boolean collisionHorizontal (Object obj1, Object obj2) {
+    public static boolean collisionHrizontal  (Object obj1, Object obj2) {
 
         int obj2Ey = obj2.getY() + obj2.getHeight();
         int obj1Ey = obj1.getY() + obj1.getHeight();
         if (((obj2.getY() <= obj1.getY()) && (obj1.getY() <= obj2Ey)) || ((obj2.getX() <= obj1Ey) && (obj1Ey <= obj2Ey))){return true;}
-        else {return false;}
-
+        else {return false;} 
     }
+    
+    public static boolean hasCollisionComplex( Object obj1, Object obj2){
+		if(collisionHrizontal(obj1,obj2) & collisionHrizontal(obj1,obj2)){return true;}
+		else{return false;}
+	}
 }
