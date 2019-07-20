4eafd4e6d885745499a9db09e24a87c582a85a4f
spoonLead <xuxlaevoleg@yandex.ru>
2019-07-20 19:59:49 +0400

    Added constructor for Camera class

--- src/com/flatgine/camera/Camera.java
+++ src/com/flatgine/camera/Camera.java
@@ -1,5 +1,22 @@
 package com.flatgine.camera;
 
 public class Camera {
-
+	private int x; 
+	private int y;
+	//TODO make variables for focusOn method: speed, acceleration
+	
+	
+	public Camera(int x,int y) {
+		setX(x);
+		setY(y);
+	}
+	
+	private void setX(int X) {
+		this.x = X;
+	}
+	
+	private void setY(int Y) {
+		this.y = Y;
+	}
+	
 }
