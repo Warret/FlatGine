e8738a6bbdbf58a9911c1f53d64662e2a16e70dd
pcePG <pcepg@MacBook-Air-pcePG.local>
2019-08-08 11:15:19 +0400

    Refactoring in Object.java

--- src/com/flatgine/objectsys/Object.java
+++ src/com/flatgine/objectsys/Object.java
@@ -6,9 +6,6 @@ public class Object {
     int modelX, modelY, modelWidth, modelHeight;
     int x, y, width, height;
 
-    //for collisions
-    int ex, ey;
-
     String state;
 
     //for physSys
@@ -17,14 +14,9 @@ public class Object {
     int maxSpeedFall;
     int accelerationFall;
 
-
-    public String getScene() {
-        return scene;
-    }
     
-    public int getCurrentSpeedFall() {
-        return currentSpeedFall;
-    }
+    
+    //_____GETTERS____
     
     public int getX() {
         return x;
@@ -40,11 +32,24 @@ public class Object {
     public int getHeight() {
         return height;
     }
-    
-    public void setCurrentSpeedFall(int currentSpeedFall) {
-        this.currentSpeedFall =currentSpeedFall ;
+
+    public int getAccelerationFall() {
+        return accelerationFall;
     }
     
+    public int getMaxSpeedFall() {
+        return maxSpeedFall;
+    }
+    
+    public int getCurrentSpeedFall() {
+        return currentSpeedFall;
+    }
+    
+    
+    
+   //_____SETTERS__INCREMENTS__DECREMENTS_____
+    
+    //__X__
     public void setX(int x) {
         this.x = x;
     }
@@ -53,6 +58,12 @@ public class Object {
         this.x += parameter;
     }
     
+    public void decreaseXBy(int parameter) {
+        this.x -= parameter;
+    }
+    
+    
+    //__Y__
     public void setY(int y) {
         this.y = y;
     }
@@ -61,63 +72,23 @@ public class Object {
         this.y += parameter;
     }
     
-    public void setWidth(int width) {
-        this.width = width;
+    public void decreaseYBy(int parameter) {
+        this.y -= parameter;
     }
     
-    public void setHeight(int height) {
-        this.height = height;
-    }
 
-    public int getModelX() {
-        return modelX;
+    //__FOR_PHYS__
+    
+    public void setCurrentSpeedFall(int currentSpeedFall) {
+        this.currentSpeedFall =currentSpeedFall ;
     }
-
-    public void setModelX(int modelX) {
-        this.modelX = modelX;
-    }
-
-    public int getModelWidth() {
-        return modelWidth;
-    }
-
-    public void setModelWidth(int modelWidth) {
-        this.modelWidth = modelWidth;
-    }
-
-    public int getEx() {
-        return ex;
-    }
-
-    public void setEx(int ex) {
-        this.ex = ex;
-    }
-
-    public boolean isPhys() {
-        return phys;
-    }
-
-    public void setPhys(boolean phys) {
-        this.phys = phys;
-    }
-
-    public int getMaxSpeedFall() {
-        return maxSpeedFall;
-    }
-
+    
     public void setMaxSpeedFall(int maxSpeedFall) {
         this.maxSpeedFall = maxSpeedFall;
-    }
-
-    public int getAccelerationFall() {
-        return accelerationFall;
     }
 
     public void setAccelerationFall(int accelerationFall) {
         this.accelerationFall = accelerationFall;
     }
 
-    public void setScene(String scene) {
-        this.scene = scene;
-    }
 }
